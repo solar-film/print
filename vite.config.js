4725d6e9c313e83e7a9bfb0cb8b5e643ad17ae33
OILLY <supaporn.gfs@gmail.com>
2026-08-09 12:11:11 +0700

    Rename icons to force PWA cache update

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
+      includeAssets: ['app-icon-192.png', 'app-icon-512.png'],
       manifest: {
         name: 'Sticker Printer Pro',
         short_name: 'StickerPrint',
@@ -21,17 +21,17 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'icon-192.png',
+            src: 'app-icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: 'app-icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: 'app-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
